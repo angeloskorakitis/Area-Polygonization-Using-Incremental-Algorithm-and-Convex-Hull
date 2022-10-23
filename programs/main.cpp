1a1d923e401aea911a6ec5b22d4d4a0ba2f19b49
korakitis <angekr4321@gmail.com>
2022-10-23 15:31:38 +0300

    MESSAGE

--- programs/main.cpp
+++ programs/main.cpp
@@ -7,6 +7,7 @@
 #include <CGAL/intersections.h>
 #include <vector>
 #include <numeric>
+#include <string>
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel   K;
 typedef K::Point_2                                            Point;
@@ -21,6 +22,8 @@ typedef std::vector<Point>                                    PointVector;
 typedef std::vector<Segment>                                  SegmentVector;
 typedef PointVector::iterator                                 pPointVector;
 typedef SegmentVector::iterator                               pSegmentVector;
+typedef std::string                                           String;
+
 
 
 //
@@ -30,6 +33,30 @@ typedef SegmentVector::iterator                               pSegmentVector;
 // παίρνουμε το ΚΠ των σημείων μας και κάνουμε iterate τα σημεία του ΚΠ 'προσπαθώντας' να προσθέσουμε σημεία στο πολύγωνο διατηρώντας το απλό
 //
 bool is_edge_visible(Point , Segment , Polygon );
+
+// class Polygonization
+// {
+//   private:
+//     const PointVector input_points;
+//     const String algorithm;
+//     const String output_file;
+//     Polygon polygon;
+//   public:
+//     Polygonization(std::string input_string);
+//     ~Polygonization();
+//     String get_algorithm()const;
+//     String get_output_file()const;
+//     Polygon get_polygon();
+//     Polygon incremental_algorithm(Polygon);
+//     Polygon convex_hull_algorithm(Polygon);
+//     void set_polygon(Polygon);
+// };
+
+
+
+
+
+
 
 int main(int argc, char *argv[])
 {
@@ -44,7 +71,7 @@ int main(int argc, char *argv[])
   
   pPointVector p_vertices = vertices.begin();
 
-  // Πρώτο κάνω sort τα σημεία με βάση μια συντεταγμένη (για την ώρα είναι σορταρισμένα στην x)
+  // Πρώτο κάνω sort τα σημεία με βάση μια συντεταγμένη (για την ώρα είναι σορταρισμένα στην x) STD::SORT
 
   // Επιλέγω αρχικό πολύγωνο - τρίγωνο, τα τρια σημεία με το μικρότερο x
   Polygon polygon(p_vertices, p_vertices+ 3);
@@ -91,17 +118,74 @@ int main(int argc, char *argv[])
 }
 
 // Ελέγχει αν ένα segment είναι ορατό από ένα σημείο
-// !!! Υπάρχει περίπτωση να χρειαστώ ένα template για τη συγκεκριμένη συνάρτηση 
+// !!! Υπάρχει περίπτωση να χρειαστώ ένα template για τη συγκεκριμένη συνάρτηση
+// update: Μαλλόν οχι απλως δημιουργία υποσυναρτησης
 bool is_edge_visible(Point point, Segment segment, Polygon convex_hull)
 {
   Point source = segment.source();
   Point target = segment.target();
 
-  // Segment segment_source(point, source);
-  // Segment segment_target(point, target);
-
   // Δημιουργώ το τρίγωνο με κορυφές Source Target Point
   Triangle triangle(point, source, target);
+
+  // Υπολογίζεις το intersection των παραπάνω segments με το polygon και θες να έχουν μόνο ένα κοινό σημείο δηλ. το σημείο source και target αντίστοιχα
+  // Για κάθε segment του CH να υπολογίζω τα σημεία που κάνει intersect
+  // Για να ξερω οτι τα δυο segments τεμνουν μονο τη μια κορυφη αρκει να ελεγξω αν τεμνουν μονο (!) τις δυο γειτονικες ακμές του ΚΠ
+  //!!! Ενδεχομένως να μπορω να παρω το τριγωνο και να δω αν υπαρχουν γραμμες του πολυγωνου που το τεμνουν !!!
+  for (EdgeIterator edge_itr = convex_hull.edges_begin(); edge_itr != convex_hull.edges_end(); ++edge_itr) 
+  {
+    Point intersection_point;
+    Segment intersection_segment;
+
+    Object result = CGAL::intersection(triangle, *edge_itr);
+    
+    // Αν η τομή τους είναι ένα σημείο...
+    if (CGAL::assign(intersection_point, result)) 
+    {
+      if(intersection_point == target) continue;
+      if(intersection_point == source) continue;
+      // !!! Δεν είμαι σίγουρος γι αυτό...να το σκεφτώ περισσότερο !!!
+      else if(intersection_point != target) return false;
+      else if(intersection_point != source) return false;
+
+    }
+    else
+    {
+      // Αν η τομή τους είναι ένα ευθύγραμμο τμήμα τότε...
+      if (CGAL::assign(intersection_segment, result)) 
+      {
+        // Αν η τομή τους είναι segment που είναι το segment τότε είναι ορατό...άρα συνεχίζουμε
+        if(intersection_segment == segment) continue;
+        
+        // Αν η τομή τους είναι segment που δεν είναι το segment τότε δεν είναι ορατό...άρα επιστροφή false
+        else if(intersection_segment != segment) return false;
+      
+      // Αν δεν υπάρχει τομή τότε...συνέχισε    
+      } 
+      else 
+        continue;
+    }
+  }
+
+  return true;
+}
+
+// OXI OLOKLHRWMENH !!!
+// Ελέγχει αν ένα segment είναι ορατό από ένα σημείο
+// !!! Υπάρχει περίπτωση να χρειαστώ ένα template για τη συγκεκριμένη συνάρτηση
+// update: Μαλλόν ναι 
+bool is_edge_visible(Segment ch_segment, Segment polygon_segment, Polygon convex_hull)
+{
+  Point polygon_source = polygon_segment.source();
+  Point polygon_target = polygon_segment.target();
+
+  Point ch_source = ch_segment.source();
+  Point ch_target = ch_segment.target();
+
+  // Δημιουργώ το τρίγωνο με κορυφές Source Target Point
+  if(polygon_source == ch_source) Triangle triangle( polygon_source, polygon_target, ch_target);
+
+  //...........................................................................................
 
   // Υπολογίζεις το intersection των παραπάνω segments με το polygon και θες να έχουν μόνο ένα κοινό σημείο δηλ. το σημείο source και target αντίστοιχα
   // Για κάθε segment του CH να υπολογίζω τα σημεία που κάνει intersect
