bdb68fd90eddfac7298a67256bb73503c0071408
korakitis <angekr4321@gmail.com>
2022-10-28 15:24:03 +0300

    messageeeee

--- programs/main.cpp
+++ programs/main.cpp
@@ -1,6 +1,5 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/convex_hull_2.h>
-#include <CGAL/Simple_cartesian.h>
 // #include <CGAL/Cartesian.h>
 #include <CGAL/Polygon_2.h>
 // #include <CGAL/property_map.h>
@@ -38,6 +37,10 @@ Polygon incremental_algorithm(PointVector );
 void print_point(Point);
 void print_segment(Segment);
 void print_polygon(Polygon);
+Segment pick_visible_edge(Point point, Polygon polygon);
+Segment pick_random_edge(Polygon polygon);
+Segment pick_max_area_edge(Point point, Polygon polygon);
+Segment pick_min_area_edge(Point point, Polygon polygon);
 
 // Να το δούμε
 // class Polygonization
@@ -92,11 +95,17 @@ int main(int argc, char *argv[])
 // // Ελέγχει αν ένα segment είναι ορατό από ένα σημείο
 bool is_edge_visible(Point point, Segment segment, Polygon convex_hull)
 {
-  Point source = segment.source();
-  Point target = segment.target();
+  Point segment_source = segment.source();
+  Point segment_target = segment.target();
+
+// Τι θα κάνουμε με τα συνευθειακά σημεία; Προς το παρόν τα συμπεριφέρομαι ως not visible.
+  if(CGAL::collinear(point, segment_source, segment_target)) {
+    std::cout << "COLLINEAR!" << std::endl;
+    return false;
+  }
 
   // Δημιουργώ το τρίγωνο με κορυφές Source Target Point
-  Triangle triangle(point, source, target);
+  Triangle triangle(point, segment_source, segment_target);
 
   // Υπολογίζεις το intersection των παραπάνω segments με το polygon και θες να έχουν μόνο ένα κοινό σημείο δηλ. το σημείο source και target αντίστοιχα
   // Για κάθε segment του CH να υπολογίζω τα σημεία που κάνει intersect
@@ -106,17 +115,17 @@ bool is_edge_visible(Point point, Segment segment, Polygon convex_hull)
   {
     Point intersection_point;
     Segment intersection_segment;
-
+    
     Object result = CGAL::intersection(triangle, *edge_itr);
     
     // Αν η τομή τους είναι ένα σημείο...
     if (CGAL::assign(intersection_point, result)) 
     {
-      if(intersection_point == target) continue;
-      if(intersection_point == source) continue;
+      if(intersection_point == segment_target) continue;
+      if(intersection_point == segment_source) continue;
       // !!! Δεν είμαι σίγουρος γι αυτό...να το σκεφτώ περισσότερο !!!
-      else if(intersection_point != target) return false;
-      else if(intersection_point != source) return false;
+      // else if(intersection_point != target) return false;
+      // else if(intersection_point != source) return false;
 
     }
     else
@@ -125,10 +134,10 @@ bool is_edge_visible(Point point, Segment segment, Polygon convex_hull)
       if (CGAL::assign(intersection_segment, result)) 
       {
         // Αν η τομή τους είναι segment που είναι το segment τότε είναι ορατό...άρα συνεχίζουμε
-        if(intersection_segment == segment) continue;
+        if(intersection_segment == segment || intersection_segment == segment.opposite()) continue;
         
         // Αν η τομή τους είναι segment που δεν είναι το segment τότε δεν είναι ορατό...άρα επιστροφή false
-        else if(intersection_segment != segment) return false;
+        else if(intersection_segment != segment || intersection_segment == segment.opposite()) return false;
       
       // Αν δεν υπάρχει τομή τότε...συνέχισε    
       } 
@@ -140,64 +149,93 @@ bool is_edge_visible(Point point, Segment segment, Polygon convex_hull)
   return true;
 }
 
-// ΟΧΙ ΟΛΟΚΛΗΡΩΜΕΝΗ !!!
-// Ελέγχει αν ένα segment είναι ορατό από ένα σημείο
-// Ιδέα: Θα μπορούσαμε να χρησιμοποιησουμε την προηγούμενη συνάρτηση στα άκρα του segment
-// bool is_edge_visible(Segment ch_segment, Segment polygon_segment, Polygon convex_hull)
-// {
-//   Point polygon_source = polygon_segment.source();
-//   Point polygon_target = polygon_segment.target();
+// _____________________________________________________________________
+Segment pick_visible_edge(Point point, Polygon polygon) 
+{
+  // Εδώ θα πρέπει να γίνεται επιλογή ανάλογα με το τι έχει δώσει ο χρήστης, μεγιστοποίηση/ελαχιστοποίηση εμβαδού κλπ. 
+  Segment edge;
 
-//   Point ch_source = ch_segment.source();
-//   Point ch_target = ch_segment.target();
+  // // Επιλέγω μία τυχαία ορατή ακμή. 
+  // while(!edge_is_visible(point, edge = pick_random_edge(polygon), polygon));
 
-//   // Δημιουργώ το τρίγωνο με κορυφές Source Target Point
-//   // if(polygon_source == ch_source) triangle( polygon_source, polygon_target, ch_target);
-//   // if(polygon_source == ch_target) triangle( polygon_source, polygon_target, ch_source);
-//   //...........................................................................................
-//   // Υπολογίζεις το intersection των παραπάνω segments με το polygon και θες να έχουν μόνο ένα κοινό σημείο δηλ. το σημείο source και target αντίστοιχα
-//   // Για κάθε segment του CH να υπολογίζω τα σημεία που κάνει intersect
-//   // Για να ξερω οτι τα δυο segments τεμνουν μονο τη μια κορυφη αρκει να ελεγξω αν τεμνουν μονο (!) τις δυο γειτονικες ακμές του ΚΠ
-//   //!!! Ενδεχομένως να μπορω να παρω το τριγωνο και να δω αν υπαρχουν γραμμες του πολυγωνου που το τεμνουν !!!
-//   Triangle triangle( polygon_source, polygon_target, ch_target);
+  // Επιλέγω την ορατή ακμή που φτιάχνει το μεγαλύτερο τρίγωνο με το σημείο.
+  edge = pick_max_area_edge(point, polygon);
 
-//   for (EdgeIterator edge_itr = convex_hull.edges_begin(); edge_itr != convex_hull.edges_end(); ++edge_itr) 
-//   {
-//     Point intersection_point;
-//     Segment intersection_segment;
+  // // Επιλέγω την ορατή ακμή που φτιάχνει το μικρότερο τρίγωνο με το σημείο. 
+  // edge = pick_min_area_edge(point, polygon);
 
-//     Object result = CGAL::intersection(triangle, *edge_itr);
+  return edge;
+  // Γίνεται να φτάσω στο τέλος χωρίς να έχω ορατή ακμή; Οέο;
+}
 
-//     // Αν η τομή τους είναι ένα σημείο...
-//     if (CGAL::assign(intersection_point, result)) 
-//     {
-//       if(intersection_point == ch_target) continue;
-//       if(intersection_point == ch_source) continue;
-//       // !!! Δεν είμαι σίγουρος γι αυτό...να το σκεφτώ περισσότερο !!!
-//       else if(intersection_point != ch_target) return false;
-//       else if(intersection_point != ch_source) return false;
 
-//     }
-//     else
-//     {
-//       // Αν η τομή τους είναι ένα ευθύγραμμο τμήμα τότε...
-//       if (CGAL::assign(intersection_segment, result)) 
-//       {
-//         // Αν η τομή τους είναι segment που είναι το segment τότε είναι ορατό...άρα συνεχίζουμε
-//         if(intersection_segment == ch_segment) continue;
-        
-//         // Αν η τομή τους είναι segment που δεν είναι το segment τότε δεν είναι ορατό...άρα επιστροφή false
-//         else if(intersection_segment != ch_segment) return false;
-      
-//       // Αν δεν υπάρχει τομή τότε...συνέχισε    
-//       } 
-//       else 
-//         continue;
-//     }
-//   }
+// Returns random edge of polygon.
+Segment pick_random_edge(Polygon polygon) {
+  int r = rand() % polygon.size();
+  return *(polygon.edges_begin() + r);
+}
 
-//   return 0;
-// }
+
+// Returns the -visible from point- edge of the polygon, that creates the largest (area-wise) triangle with the given point.
+Segment pick_max_area_edge(Point point, Polygon polygon) {
+  Segment max_segment;
+  double max_area = -1;
+  double current_area;
+
+  for(EdgeIterator edge = polygon.edges_begin(); edge != polygon.edges_end(); edge++) {
+
+
+    // If the edge is not visible, skip the edge. 
+   if(!is_edge_visible(point, *edge, polygon)) continue;
+
+
+    Triangle triangle((*edge).source(), (*edge).target(), point);
+    // area() function may return negative number.
+    current_area = CGAL::abs(triangle.area());
+
+    if(current_area > max_area) {
+      max_segment = *edge;
+      max_area = current_area;
+    }
+
+  }
+
+
+  return max_segment;
+}
+
+
+// Returns the -visible from point- edge of the polygon, that creates the smallest (area-wise) triangle with the given point.
+Segment pick_min_area_edge(Point point, Polygon polygon) {
+  Segment min_segment;
+  double min_area;
+  double current_area;
+
+  for(EdgeIterator edge = polygon.edges_begin(); edge != polygon.edges_end(); edge++) {
+
+    // If the edge is not visible, skip the edge. 
+    if(!is_edge_visible(point, *edge, polygon)) continue;
+
+    Triangle triangle((*edge).source(), (*edge).target(), point);
+    // area() function may return negative number.
+    current_area = CGAL::abs(triangle.area());
+
+    if(edge == polygon.edges_begin()) {
+      min_area = current_area;
+      min_segment = *edge;
+    }
+
+    if(current_area < min_area) {
+      min_segment = *edge;
+      min_area = current_area;
+    }
+
+  }
+
+  return min_segment;
+}
+// _____________________________________________________________________
+
 
 // ΟΧΙ ΟΛΟΚΛΗΡΩΜΕΝΗ !!!
 // Η υλοποίηση του αυξητικού αλγορίθμου
@@ -216,29 +254,33 @@ Polygon incremental_algorithm(PointVector input_points)
   {
     // Υπολογισμός του ΚΠ
     CGAL::convex_hull_2(polygon.begin(), polygon.end(), std::back_inserter(convex_hull));
-  
 
     // Vector που θα περιέχει τις κόκκινες ακμές
     SegmentVector red_edges;
 
-    // Για κάθε ακμή του ΚΠ, επανάλαβε...
+    // Βρες τις κόκκινες ακμές...
     for (EdgeIterator edge_itr = convex_hull.edges_begin(); edge_itr != convex_hull.edges_end(); ++edge_itr) 
     {
-      if(is_edge_visible(*p_input_points, *edge_itr, convex_hull) == true) red_edges.push_back(*edge_itr);
+      if(is_edge_visible(*p_input_points, *edge_itr, convex_hull))
+      {
+        red_edges.push_back(*edge_itr);
+      } 
     }
 
     pSegmentVector p_red_edges;
 
-    // // Αν υπάρχουν κόκκινες ακμές στο ΚΠ δηλ. ορατές από το σημείο...
-    // if(red_edges.size() != 0)
-    // {
-    //   // Για κάθε μία βρίσκω τις ορατές ακμές 
-    //   for(p_red_edges = red_edges.begin(); p_red_edges != red_edges.end(); ++p_red_edges)
-    //   {
-    //     // Έστω ότι η επιλογή ορατή ακμής γίνεται τυχαία...(δηλαδή επιλέγω την πρώτη ακμή)
-    //     // Για κάθε κόκκινη πρέπει να βρώ ποιες γραμμές είναι ορατές...
-    //   }
-    // }
+    // Αν υπάρχουν κόκκινες ακμές στο ΚΠ δηλ. ορατές από το σημείο...
+    if(red_edges.size() != 0)
+    {
+      // Για κάθε μία κόκκινη ακμή βρίσκω τις ορατές ακμές στο πολύγωνο...
+      for(p_red_edges = red_edges.begin(); p_red_edges != red_edges.end(); ++p_red_edges)
+      {
+        // Για κάθε κόκκινη πρέπει να βρώ ποιες γραμμές είναι ορατές...
+        // Κάθε κόκκινη θα έχει τις κορυφές της στην πολυγωνική γραμμή...
+        // EEEXW MPERDEUTEIII
+
+      }
+    }
 
     // Για κάθε ορατή ακμή του ΚΠ θέλω να βρώ τις ορατές ακμές στο πολύγωνο και ανάλογα με το strategy (τυχαία επιλογή, μέγιστο, ελάχστο εμβαδόν) να τις επιλέξω...
     
