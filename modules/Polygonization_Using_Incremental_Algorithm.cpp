e255e73608988bd6e0d035d145476fdd6cde209b
korakitis <angekr4321@gmail.com>
2022-11-04 21:43:53 +0200

    Algorithm running correctly... tested with 100, 300, 500, 1000, 2000 points with different arguments. To-do: 1.Documention/ Comments 2. Code in one file 3. Send it

--- modules/Polygonization_Using_Incremental_Algorithm.cpp
+++ modules/Polygonization_Using_Incremental_Algorithm.cpp
@@ -8,50 +8,79 @@
 
 #include "Polygonization_Using_Incremental_Algorithm.hpp"
 
-bool is_edge_visible(Point point, Segment segment, Polygon convex_hull)
+bool x_increasing(const Point a, const Point b){
+    return a.x() <= b.x(); 
+}
+
+bool x_decreasing(const Point a, const Point b){
+    return a.x() >= b.x(); 
+}
+
+bool y_increasing(const Point a, const Point b){
+    return a.y() <= b.y(); 
+}
+
+bool y_decreasing(const Point a, const Point b){
+    return a.y() >= b.y(); 
+}
+
+void sort_input_points(PointVector* input_points, String initialization)
+{
+  if(initialization == "1a")
+    std::sort(input_points->begin(), input_points->end(), x_decreasing);
+  else if(initialization == "1b")
+    std::sort(input_points->begin(), input_points->end(), x_increasing);
+  else if(initialization == "2a")
+    std::sort(input_points->begin(), input_points->end(), y_decreasing);
+  else if(initialization == "2b")
+    std::sort(input_points->begin(), input_points->end(), y_increasing);
+}
+
+bool is_edge_visible(Point point, Segment segment, Polygon polygon)
 {
   Point segment_source = segment.source();
   Point segment_target = segment.target();
 
-  if(CGAL::collinear(point, segment_source, segment_target)) {
-    // std::cout << "COLLINEAR!" << std::endl;
-    return false;
-  }
+  // Special case: If the 3 points are collinear then the segment is not visible
+  if(CGAL::collinear(point, segment_source, segment_target))
 
-  // Δημιουργώ το τρίγωνο με κορυφές Source Target Point
+    return false;
+
+  // Create a triangle using the 3 the segment and the point given
   Triangle triangle(point, segment_source, segment_target);
 
   // Υπολογίζεις το intersection των παραπάνω segments με το polygon και θες να έχουν μόνο ένα κοινό σημείο δηλ. το σημείο source και target αντίστοιχα
   // Για κάθε segment του CH να υπολογίζω τα σημεία που κάνει intersect
   // Για να ξερω οτι τα δυο segments τεμνουν μονο τη μια κορυφη αρκει να ελεγξω αν τεμνουν μονο (!) τις δυο γειτονικες ακμές του ΚΠ
   //!!! Ενδεχομένως να μπορω να παρω το τριγωνο και να δω αν υπαρχουν γραμμες του πολυγωνου που το τεμνουν !!!
-  for (EdgeIterator edge_itr = convex_hull.edges_begin(); edge_itr != convex_hull.edges_end(); ++edge_itr) 
+  for (EdgeIterator edge_itr = polygon.edges_begin(); edge_itr != polygon.edges_end(); ++edge_itr) 
   {
     Point intersection_point;
     Segment intersection_segment;
     
     Object result = CGAL::intersection(triangle, *edge_itr);
     
-    // Αν η τομή τους είναι ένα σημείο...
+    // If the intersection is a point...
     if (CGAL::assign(intersection_point, result)) 
     {
+      // If that point is the segment points the continue
       if(intersection_point == segment_target) continue;
       if(intersection_point == segment_source) continue;
+      // Else, not visible
       return false;
     }
     else
     {
-      // Αν η τομή τους είναι ένα ευθύγραμμο τμήμα τότε...
+      // If intersection is a segment...
       if (CGAL::assign(intersection_segment, result)) 
       {
-        // Αν η τομή τους είναι segment που είναι το segment τότε είναι ορατό...άρα συνεχίζουμε
+        // If the intersection is the segment then it is visible...continue
         if(intersection_segment == segment || intersection_segment == segment.opposite()) continue;
         
-        // Αν η τομή τους είναι segment που δεν είναι το segment τότε δεν είναι ορατό...άρα επιστροφή false
-        else if(intersection_segment != segment || intersection_segment == segment.opposite()) return false;
-      
-      // Αν δεν υπάρχει τομή τότε...συνέχισε    
-      } 
+        // If not, the point in not visible
+        else if(intersection_segment != segment || intersection_segment != segment.opposite()) return false;
+      }
+      // If no intersection at all the continue...
       else 
         continue;
     }
@@ -76,20 +105,23 @@ void add_visible_edge(Point point, SegmentVector visible_polygon_edges, int edge
 }
 
 
-SegmentVector find_red_edges(Point point, Polygon polygon)
+SegmentVector find_red_edges(Point point, Polygon polygon, String initialization)
 {
   Polygon convex_hull;
-  // Υπολογισμός του ΚΠ
+  // Calculate the CH
   CGAL::convex_hull_2(polygon.begin(), polygon.end(), std::back_inserter(convex_hull));
 
-  // Vector που θα περιέχει τις κόκκινες ακμές
+  // if(convex_hull.is_clockwise_oriented())
+  //   convex_hull.reverse_orientation();
+
   SegmentVector red_edges;
 
-  // Βρες τις κόκκινες ακμές...
+  // Find visible edges
   for (EdgeIterator edge_itr = convex_hull.edges_begin(); edge_itr != convex_hull.edges_end(); ++edge_itr) 
   {
     if(is_edge_visible(point, *edge_itr, convex_hull))
     {
+      // if(*edge_itr->direction())
       red_edges.push_back(*edge_itr);
     } 
   }
@@ -101,43 +133,45 @@ SegmentVector find_red_edges(Point point, Polygon polygon)
 SegmentVector find_visible_edges(Point point, SegmentVector red_edges, Polygon polygon)
 {
   SegmentVector visible_edges;
-  // Αν υπάρχουν κόκκινες ακμές στο ΚΠ δηλ. ορατές από το σημείο...
+  
   if(red_edges.size() != 0)
   {      
     pSegmentVector p_red_edges_begin = red_edges.begin();
-    pSegmentVector p_red_edges_end = red_edges.end();
+    // pSegmentVector p_red_edges_end = red_edges.end();
 
-    // Special case: already in the edge
+    // Special case: Point intersects a red edge so the only edge visible is that red edge
     if(CGAL::do_intersect(*p_red_edges_begin, point)){
       return red_edges;
     }
     
-      // Για κάθε κόκκινη πρέπει να βρώ ποιες ακμές είναι ορατές...
-      // Κάθε κόκκινη θα έχει τις κορυφές της στην πολυγωνική γραμμή...
-      bool flag = false;
-      for(EdgeIterator edge_itr = polygon.edges_begin(); edge_itr != polygon.edges_end(); ++edge_itr)
-      {
-        // Αν η κορυφή του πολυγώνου είναι η κορυφή της κόκκινης ακμής...
-        if(edge_itr->source() == p_red_edges_begin->source() || flag == true)
-        {
-          // We found the first red edge
-          flag = true;
+    // A boolean that indecates if we found the first red edge
+    // bool flag = false;
+    for(pSegmentVector p_red_edges = red_edges.begin(); p_red_edges!=red_edges.end(); ++p_red_edges)
+    {
+      EdgeCirculator edge_circ = polygon.edges_circulator();
+      while(edge_circ->source()!=p_red_edges_begin->source()) ++edge_circ;
 
-          // if the edge is visible from the point...
-          if(is_edge_visible(point, *edge_itr, polygon))
-            visible_edges.push_back(*edge_itr);
-        }
-        if(edge_itr->target() == p_red_edges_end->target() && flag == true)
-          break;
-        
+      do{
+      // if the edge is visible from the point...
+      if(is_edge_visible(point, *edge_circ, polygon))
+        visible_edges.push_back(*edge_circ);
+      edge_circ++;
       }
+      while(edge_circ->source()!=p_red_edges->target());
     }
+  }
+
   return visible_edges;
 }
 
 
-Polygon incremental_algorithm(PointVector input_points, int edge_selection)
+Polygon incremental_algorithm(PointVector input_points, int edge_selection, String initialization)
 {
+  // Sorting the points according to <initialization>
+  sort_input_points(&input_points, initialization);
+
+  // print_point_vector(input_points);
+
   pPointVector p_input_points = input_points.begin();
 
   // Start with the first 3 points
@@ -146,10 +180,11 @@ Polygon incremental_algorithm(PointVector input_points, int edge_selection)
   // We need CCW orientation for the polygon points
   if(polygon.is_clockwise_oriented())
     polygon.reverse_orientation();
+    
 
-  // SPECIAL CASE: Check if the first 3 points are collinear...add a 4th
-  // Not ready yeeet!
-  if(CGAL::collinear(input_points.at(0), input_points.at(1), input_points.at(2))){
+  // Special case: Check if the first 3 points are collinear...add a 4th
+  if(CGAL::collinear(input_points.at(0), input_points.at(1), input_points.at(2)))
+  {
     VertexIterator itr = polygon.vertices_begin();
     polygon.insert(itr, input_points.at(3));
     advance(p_input_points, 1);
@@ -160,11 +195,16 @@ Polygon incremental_algorithm(PointVector input_points, int edge_selection)
   // While the number of the input points is different of the size of the polygon, repeat...
   while(input_points.size() != polygon.size())
   {
+    // std::cout << polygon.is_simple() << "\n";
+    // print_polygon(polygon); 
     // Find the red edges of the CH, i.e the visible from the CH
-    SegmentVector red_edges = find_red_edges(*p_input_points, polygon);
-
+    SegmentVector red_edges = find_red_edges(*p_input_points, polygon, initialization);
+    // std::cout << "red\n";
+    // print_segment_vector(red_edges);
     // Find the visible edges of the Polygon
     SegmentVector visible_edges = find_visible_edges(*p_input_points, red_edges, polygon);
+    // std::cout << "vis\n";
+    // print_segment_vector(visible_edges);
 
     // For every visible edge, insert a vertex in the polygon according to the edge selection strategy (random, min/ max area)
     add_visible_edge(*p_input_points, visible_edges, edge_selection, &polygon);
@@ -177,10 +217,11 @@ Polygon incremental_algorithm(PointVector input_points, int edge_selection)
 }
 
 
-//_____________Edge selection functions according to the input <strategy>_________
+//_____________Edge selection functions according to the input strategy <edge_selection>_________
 
 Segment pick_edge(Point point ,SegmentVector visible_polygon_edges,int edge_selection){
-  switch(edge_selection){
+  switch(edge_selection)
+  {
   case 1: return pick_random_edge(visible_polygon_edges); break;
   case 2: return pick_min_area_edge(point, visible_polygon_edges); break;
   }
@@ -189,17 +230,21 @@ Segment pick_edge(Point point ,SegmentVector visible_polygon_edges,int edge_sele
 
 Segment pick_max_area_edge(Point point ,SegmentVector visible_polygon_edges) {
   Segment max_segment;
-  double max_area = -1;
-  double current_area;
+  unsigned int max_area = -1;
+  unsigned int current_area;
 
-  for(pSegmentVector edge = visible_polygon_edges.begin(); edge != visible_polygon_edges.end(); edge++) {
+  for(pSegmentVector edge_itr = visible_polygon_edges.begin(); edge_itr != visible_polygon_edges.end(); ++edge_itr) {
 
-    Triangle triangle(edge->source(), edge->target(), point);
-    // area() function may return negative number.
+    Triangle triangle(edge_itr->source(), edge_itr->target(), point);
     current_area = CGAL::abs(triangle.area());
 
+    if(edge_itr == visible_polygon_edges.begin()) {
+      max_area = current_area;
+      max_segment = *edge_itr;
+    }
+
     if(current_area > max_area) {
-      max_segment = *edge;
+      max_segment = *edge_itr;
       max_area = current_area;
     }
 
@@ -210,22 +255,21 @@ Segment pick_max_area_edge(Point point ,SegmentVector visible_polygon_edges) {
 
 Segment pick_min_area_edge(Point point ,SegmentVector visible_polygon_edges) {
   Segment min_segment;
-  double min_area = -1;
-  double current_area;
+  unsigned int min_area = -1;
+  unsigned int current_area;
 
-  for(pSegmentVector edge = visible_polygon_edges.begin(); edge != visible_polygon_edges.end(); edge++) {
+  for(pSegmentVector edge_itr = visible_polygon_edges.begin(); edge_itr != visible_polygon_edges.end(); ++edge_itr) {
 
-    Triangle triangle(edge->source(), edge->target(), point);
-    // area() function may return negative number.
+    Triangle triangle(edge_itr->source(), edge_itr->target(), point);
     current_area = CGAL::abs(triangle.area());
 
-    if(edge == visible_polygon_edges.begin()) {
+    if(edge_itr == visible_polygon_edges.begin()) {
       min_area = current_area;
-      min_segment = *edge;
+      min_segment = *edge_itr;
     }
 
     if(current_area < min_area) {
-      min_segment = *edge;
+      min_segment = *edge_itr;
       min_area = current_area;
     }
 
@@ -244,17 +288,20 @@ Segment pick_random_edge(SegmentVector visible_polygon_edges) {
 
 
 void print_point(Point point) {
-  std::cout << "(" << point.x() << ", " << point.y() << ")" << std::endl;
+  std::cout << point.x() << ", " << point.y() << std::endl;
 }
 
-void print_segment(Segment seg) {
-  std::cout << "(" << seg.source().x() << ", " << seg.source().y() << ") - (" << seg.target().x() << ", " << seg.target().y() << ")" << std::endl;
+
+void print_segment(Segment segment) {
+  std::cout << segment.source().x() << "," << segment.source().y() <<  " - " << segment.target().x() << "," << segment.target().y() << std::endl;
 }
+
 
 void print_polygon(Polygon polygon) {
   for (EdgeIterator edge_itr = polygon.edges_begin(); edge_itr != polygon.edges_end(); ++edge_itr) 
     print_segment(*edge_itr);
 }
+
 
 void print_point_vector(PointVector points) {
   for(Point point : points) {
@@ -262,11 +309,13 @@ void print_point_vector(PointVector points) {
   }
 }
 
+
 void print_segment_vector(SegmentVector segments) {
   for(Segment segment : segments) {
     print_segment(segment);
   }
 }
+
 
 PointVector parse_file(std::string filename) {
   std::ifstream infile(filename);
@@ -291,41 +340,50 @@ return points;
 }
 
 
-// Output
-void print_output(Polygon polygon, PointVector points, std::string filename, std::string algorithm, int edge_selection, std::string initialization, auto duration) {
+void print_output(Polygon polygon, PointVector points, std::string filename, std::string algorithm, int edge_selection, std::string initialization, std::chrono::duration<double> duration) {
   std::ofstream outfile(filename);
+  try{
+    if(outfile.is_open()) {
+      outfile << "Polygonization" << std::endl;
 
-  if(outfile.is_open()) {
-    outfile << "Polygonization" << std::endl;
+      for(Point point : points) {
+        outfile << point << std::endl;
+      }
 
-    for(Point point : points) {
-      outfile << point << std::endl;
+      for(EdgeIterator edge = polygon.edges_begin(); edge != polygon.edges_end(); edge++) {
+        outfile << *edge << std::endl;
+      }
+
+
+      if(algorithm == "incremental") {
+        outfile << "Algorithm:" << algorithm << "_edge_selection" << edge_selection << "_initialization" << initialization << std::endl;
+      }
+      else {
+        outfile << "Algorithm:" << algorithm << "_edge_selection" << edge_selection << std::endl;
+      }
+
+      unsigned int polygon_area = CGAL::abs(polygon.area());
+      
+      Polygon convex_hull;
+      CGAL::convex_hull_2(polygon.begin(), polygon.end(), std::back_inserter(convex_hull));
+
+      unsigned int convex_hull_area = CGAL::abs(convex_hull.area());
+
+      double ratio = (double)polygon_area / (double)convex_hull_area;
+
+      outfile << "area: " << polygon_area << std::endl;
+      outfile << "ratio: " <<  ratio << std::endl;
+      outfile << "construction time: " << duration.count() << std::endl;
+
+      outfile.close();
+
     }
-
-    for(EdgeIterator edge = polygon.edges_begin(); edge != polygon.edges_end(); edge++) {
-      outfile << *edge << std::endl;
-    }
-
-
-    if(algorithm == "incremental") {
-      outfile << "Algorithm:" << algorithm << "_edge_selection" << edge_selection << "_initialization" << initialization << std::endl;
-    }
-    else {
-      outfile << "Algorithm:" << algorithm << "_edge_selection" << edge_selection << std::endl;
-    }
-
-    double polygon_area = CGAL::abs(polygon.area());
-    
-    Polygon convex_hull;
-    CGAL::convex_hull_2(polygon.begin(), polygon.end(), std::back_inserter(convex_hull));
-
-    double convex_hull_area = CGAL::abs(convex_hull.area());
-
-    double ratio = polygon_area / convex_hull_area;
-
-    outfile << "area: " << polygon_area << std::endl;
-    outfile << "ratio: " <<  ratio << std::endl;
-    outfile << "construction time: " << duration.count() << std::endl;
-
-
+    else throw "Output file is not open.\n";
   }
+  catch(String error_msg)
+  {
+    std::cerr << error_msg << std::endl;
+  }  
+
+  
+}
